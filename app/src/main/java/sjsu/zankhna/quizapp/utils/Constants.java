1a12d41f6d0ab95249135f1502e88068ac116aa4
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-04-08 23:48:25 -0700

    Defined api base url

--- app/src/main/java/sjsu/zankhna/quizapp/utils/Constants.java
+++ app/src/main/java/sjsu/zankhna/quizapp/utils/Constants.java
@@ -2,11 +2,15 @@ package sjsu.zankhna.quizapp.utils;
 
 public class Constants {
 
+    // API Url
+    public static String API_URL = "https://opentdb.com/api.php";
+
     // API request parameters
     public static String PARAM_AMOUNT = "amount";
     public static String PARAM_CATEGORY = "category";
     public static String PARAM_DIFFICULTY = "difficulty";
     public static String PARAM_TYPE = "type";
+
 
 
 }
