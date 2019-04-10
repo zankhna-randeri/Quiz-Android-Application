e3acbe0bc5b92237d4ef03128485d9348dcbc8db
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-04-09 18:35:50 -0700

    Declared load quiz service params

--- app/src/main/java/sjsu/zankhna/quizapp/utils/Constants.java
+++ app/src/main/java/sjsu/zankhna/quizapp/utils/Constants.java
@@ -2,15 +2,13 @@ package sjsu.zankhna.quizapp.utils;
 
 public class Constants {
 
-    // API Url
-    public static String API_URL = "https://opentdb.com/api.php";
-
     // API request parameters
     public static String PARAM_AMOUNT = "amount";
     public static String PARAM_CATEGORY = "category";
     public static String PARAM_DIFFICULTY = "difficulty";
     public static String PARAM_TYPE = "type";
 
-
-
+    // App constants
+    public static final String RESPONSE_QUESTION_SERVICE = "responseQuestionService";
+    public static final String PAYLOAD_QUESTION_SERVICE = "payloadQuestionService";
 }
