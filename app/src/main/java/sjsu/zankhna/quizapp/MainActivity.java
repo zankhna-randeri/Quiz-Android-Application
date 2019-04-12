bd6841600115c52bcbc1841280a1a2f52c3cc68f
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-04-11 17:05:27 -0700

    Added code to set cutom toolbar and title

--- app/src/main/java/sjsu/zankhna/quizapp/MainActivity.java
+++ app/src/main/java/sjsu/zankhna/quizapp/MainActivity.java
@@ -1,13 +1,48 @@
 package sjsu.zankhna.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context activityContext;
+
+    @BindView(R.id.btn_play)
+    Button play;
+
+    @BindView(R.id.btn_score)
+    Button score;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.toolbar_title)
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        activityContext = MainActivity.this;
+        setUpToolbar();
     }
+
+    private void setUpToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        title.setTypeface(ResourcesCompat.getFont(activityContext, R.font.avenir_lt_std_black));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
 }
