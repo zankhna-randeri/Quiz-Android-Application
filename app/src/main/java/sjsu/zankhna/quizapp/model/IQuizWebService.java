2d44ecf95c1c76717d01cf82ba95d53906b0f141
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-04-15 23:22:19 -0700

    Resolved api name spell-mistake

--- app/src/main/java/sjsu/zankhna/quizapp/model/IQuizWebService.java
+++ app/src/main/java/sjsu/zankhna/quizapp/model/IQuizWebService.java
@@ -19,12 +19,12 @@ public interface IQuizWebService {
 
     @GET(API_URL)
     Call<RequestQuestionResponse> loadQuestions(@Query(Constants.PARAM_AMOUNT) int amount,
-                                            @Query(Constants.PARAM_CATEGORY) int category,
-                                            @Query(Constants.PARAM_DIFFICULTY) String difficulty,
-                                            @Query(Constants.PARAM_TYPE) String type);
+                                                @Query(Constants.PARAM_CATEGORY) int category,
+                                                @Query(Constants.PARAM_DIFFICULTY) String difficulty,
+                                                @Query(Constants.PARAM_TYPE) String type);
 
     @GET(API_URL)
-    Call<RequestQuestionResponse> loadQuestionsOfAnyDiifficutly(@Query(Constants.PARAM_AMOUNT) int amount,
-                                                @Query(Constants.PARAM_CATEGORY) int category,
-                                                @Query(Constants.PARAM_TYPE) String type);
+    Call<RequestQuestionResponse> loadQuestionsOfAnyDifficulty(@Query(Constants.PARAM_AMOUNT) int amount,
+                                                               @Query(Constants.PARAM_CATEGORY) int category,
+                                                               @Query(Constants.PARAM_TYPE) String type);
 }
