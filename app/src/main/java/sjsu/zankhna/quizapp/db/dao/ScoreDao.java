08199791f571c298d5400eefe320fd32eca7019b
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-04-18 13:07:12 -0700

    DAO for performing DB operations on score table

--- app/src/main/java/sjsu/zankhna/quizapp/db/dao/ScoreDao.java
+++ app/src/main/java/sjsu/zankhna/quizapp/db/dao/ScoreDao.java
@@ -1,6 +1,9 @@
 package sjsu.zankhna.quizapp.db.dao;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
 
 import java.util.List;
 
@@ -9,8 +12,10 @@ import sjsu.zankhna.quizapp.db.entity.ScoreEntity;
 @Dao
 public interface ScoreDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertScore(ScoreEntity score);
 
+    @Query("SELECT * from score ORDER BY date DESC")
     List<ScoreEntity> getAllScores();
 
 }
