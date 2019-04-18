b8c53b7399cc49280e8d3eca9ecc9543eb4200ee
Zankhna Randeri <zankhna.randeri@gmail.com>
2019-04-18 13:06:51 -0700

    Entity class for table score

--- app/src/main/java/sjsu/zankhna/quizapp/db/entity/ScoreEntity.java
+++ app/src/main/java/sjsu/zankhna/quizapp/db/entity/ScoreEntity.java
@@ -10,10 +10,56 @@ public class ScoreEntity {
 
     @PrimaryKey(autoGenerate = true)
     private int id;
-
     private Date date;
     private int score;
     private String category;
 
-    
+
+    public ScoreEntity(int score, String category) {
+        this.date = new Date();
+        this.score = score;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "ScoreEntity{" +
+                "id=" + id +
+                ", date=" + date +
+                ", score=" + score +
+                ", category='" + category + '\'' +
+                '}';
+    }
 }
